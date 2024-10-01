081b8424f052cfd15711d7f451c4213a3f1127d4
Rodrigo <rodrigofontoura06@gmail.com>
2024-10-01 11:46:53 -0300

    reevisãoparte2

--- Site-Me-Ajude/Site do Me Ajude! html css e js/JavaScript/java.js
+++ Site-Me-Ajude/Site do Me Ajude! html css e js/JavaScript/java.js
@@ -106,3 +106,14 @@ function generateQRCode() {
     }
 });*/
 
+
+            function previewImage(event) {
+              var reader = new FileReader();
+              reader.onload = function(){
+                var output = document.getElementById('preview');
+                output.src = reader.result;
+                output.style.display = 'block';
+              };
+              reader.readAsDataURL(event.target.files[0]);
+            }
+       
